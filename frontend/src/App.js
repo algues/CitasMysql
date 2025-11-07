092ac7dee305953d26278d12a40509b3c04f109a
Alfredo Guerrero <alf10guerra68@hotmail.com>
2025-11-06 19:21:44 -0500

    App.js modificado del frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,17 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home';
 import AddPacientes from './components/pacientes/addPacientes';
+import ConsultaPacientes from './components/pacientes/consultPacientes';
+import ModificarPacientes from './components/pacientes/updatePacientes';
+import EliminarPacientes from './components/pacientes/deletePacientes';
+import AddMedicos from './components/medicos/addMedicos';
+import ConsultarMedicos from './components/medicos/consultMedicos';
+import ModificarMedicos from './components/medicos/updateMedicos';
+import EliminarMedicos from './components/medicos/deleteMedicos';
+import AddConsultorios from './components/consultorios/addConsultorios';
+import ConsultarConsultorios from './components/consultorios/consultConsultorio';
+import ModificarConsultorios from './components/consultorios/updateConsultorio';
+import EliminarConsultorios from './components/consultorios/deleteConsultorios';
 
 
 function App() {
@@ -11,7 +22,18 @@ function App() {
        <Router>
         <Routes> 
           <Route  exact path='/' element={<Home />}></Route>  
-          <Route  path='/addPacientes' element={<AddPacientes />}></Route>                       
+          <Route  path='/addPacientes' element={<AddPacientes />}></Route> 
+          <Route  path='/consultaPacientes' element={<ConsultaPacientes />}></Route>  
+          <Route  path='/modificarPacientes' element={<ModificarPacientes />}></Route> 
+          <Route  path='/eliminarPacientes' element={<EliminarPacientes />}></Route>  
+          <Route  path='/addMedicos' element={<AddMedicos />}></Route> 
+          <Route  path='/consultarMedicos' element={<ConsultarMedicos />}></Route> 
+          <Route  path='/modificarMedicos' element={<ModificarMedicos />}></Route> 
+          <Route  path='/eliminarMedicos' element={<EliminarMedicos />}></Route>  
+          <Route  path='/addConsultorios' element={<AddConsultorios />}></Route> 
+          <Route  path='/consultarConsultorios' element={<ConsultarConsultorios />}></Route> 
+          <Route  path='/modificarConsultorios' element={<ModificarConsultorios />}></Route> 
+          <Route  path='/eliminarConsultorios' element={<EliminarConsultorios />}></Route>    
         </Routes>
       </Router>     
       </header>
